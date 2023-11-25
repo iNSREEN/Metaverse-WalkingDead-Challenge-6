a36cb47cc9844f8f3e3d415a5497208c7eac36b5
Nsreen Alfaifi <116817889+iNSREEN@users.noreply.github.com>
2023-11-25 09:47:10 +0300

    Add player script movment

---
+++ Assets/Scripts/PlayerPunch.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPunch : MonoBehaviour
+{
+    [Header("Player Punch Var")]
+    public Camera cam;
+    public float giveDamageOf = 10f;
+    public float punchingRange = 5f;
+
+
+    public void Punch()
+    {
+        RaycastHit hitInfo; // store infromation about object we hit
+
+/*        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, punchingRange))
+        {
+            Debug.Log(hitInfo.transform.name);//show name iof whatever hit
+
+            ObjectToHit objectToHit = hitInfo.transform.GetComponent<ObjectToHit>(); //from other script opjecttohit
+            Zombi1 zombi1 = hitInfo.transform.GetComponent<Zombi1>();
+            Zombi2 zombi2 = hitInfo.transform.GetComponent<Zombi2>();
+
+            if (objectToHit != null) // shoot object 
+            {
+                objectToHit.ObjectHitDamage(giveDamageOf);
+
+            }
+            else if (zombi1 != null)
+            {
+                zombi1.zombiHitDamage(giveDamageOf);
+            }
+            else if (zombi2 != null)
+            {
+                zombi2.zombiHitDamage(giveDamageOf);
+            }
+
+        }*/
+
+    }
+}
