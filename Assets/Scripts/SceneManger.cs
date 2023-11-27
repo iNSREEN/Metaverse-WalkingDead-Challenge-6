62a70d134ab9d90b0eae5961e8ecf2e823837ac7
Nsreen Alfaifi <116817889+iNSREEN@users.noreply.github.com>
2023-11-28 00:52:58 +0300

    update

---
+++ Assets/Scripts/SceneManger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManger : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
